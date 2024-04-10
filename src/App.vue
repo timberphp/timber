c1e0e5442b794e643a16d9518ff70f336ac06f13
bangnokia <bangnokia@gmail.com>
2024-04-10 18:52:32 +0700

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { Button } from './components/ui/button';
 import { Textarea } from './components/ui/textarea';
+
+const phpBinary = 'php';
+const code = ref(`echo "hello world";`);
+const result = ref('');
+
+function submit() {
+
+}
 </script>
 
 <template>
   <div class="p-6 container mx-auto items-center justify-center">
-    <Textarea autofocus class="font-mono h-32" />
-    <Button class="mt-4">Submit</Button>
+    <form @submit.prevent="submit">
+      <Textarea v-model="code" autofocus class="font-mono h-32" />
+      <Button type="submit" class="mt-4">Submit</Button>
+    </form>
+    <div v-html="result"></div>
   </div>
 </template>
