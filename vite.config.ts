27bc76f279f02e528574639445404b2e293886c9
bangnokia <bangnokia@gmail.com>
2024-04-10 18:12:04 +0700

    add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -16,6 +24,11 @@ export default defineConfig(async () => ({
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
