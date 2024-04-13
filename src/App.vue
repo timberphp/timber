f9bd087cf6cd2a315ba7ee37b0a4a320e6462ea6
bangnokia <bangnokia@gmail.com>
2024-04-13 23:59:16 +0700

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,7 @@
 <script setup lang="ts">
-import {ref} from 'vue';
-import {Button} from './components/ui/button';
-import {Textarea} from './components/ui/textarea';
-import {invoke} from '@tauri-apps/api/core';
-import {makeCommand} from './lib/code-runner';
-import {open} from '@tauri-apps/plugin-dialog'
-
-const code = ref(`echo "hello world";`);
-const directory = ref('/Users/daudau/Code/lab');
-const output = ref('');
-
-
-async function submit() {
-  const command = await makeCommand(directory.value, 'local', code.value);
-  console.log(command);
-  try {
-    const result = await invoke('execute_command', {command}) as string;
-    output.value = result;
-  } catch (e) {
-    if (typeof e === 'string') {
-      output.value = e;
-    } else if (e instanceof Error) {
-      output.value = e.message;
-    } else {
-      output.value = "An error occurred."
-    }
-  }
-}
-
-async function selectFolder() {
-  const selected = await open({
-    directory: true,
-    multiple: false,
-  })
-  if (selected) {
-    directory.value = selected;
-  }
-}
+import Nav from "./Nav.vue"
 </script>
 
 <template>
-  <div class="p-6 container mx-auto items-center justify-center">
-    <div>
-      Directory: {{ directory }}
-      <Button variant="outline" type="button" @click="selectFolder">select directory</Button>
-    </div>
-    <form @submit.prevent="submit">
-      <Textarea v-model="code" autofocus class="font-mono h-32"/>
-      <Button type="submit" class="mt-4">Submit</Button>
-    </form>
-    <pre class="text-sm" v-text="output"></pre>
-  </div>
+  <Nav />
 </template>
---
+++ src/Nav.vue
@@ -0,0 +1,60 @@
+<script setup lang=ts>
+import { Icon } from '@iconify/vue';
+import { cn } from './lib/utils'
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from '@/components/ui/tooltip'
+
+export interface LinkProp {
+    title: string
+    label?: string
+    icon: string
+    variant: 'default' | 'ghost'
+}
+
+const links: LinkProp[] = [
+    {
+        title: 'Open Folder',
+        icon: 'lucide:inbox',
+        variant: 'default',
+    },
+    {
+        title: 'Drafts',
+        icon: 'lucide:file',
+        variant: 'ghost',
+    },
+    {
+        title: 'Sent',
+        icon: 'lucide:send',
+        variant: 'ghost',
+    },
+    {
+        title: 'Junk',
+        icon: 'lucide:archive',
+        variant: 'ghost',
+    },
+    {
+        title: 'Trash',
+        icon: 'lucide:trash',
+        variant: 'ghost',
+    },
+    {
+        title: 'Archive',
+        icon: 'lucide:archive',
+        variant: 'ghost',
+    },
+]
+</script>
+
+<template>
+    <div class="group flex flex-col gap-4 py-2 data-[collapsed=true]:py-2">
+        <nav class="grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
+                <a href="#" class="size-9">
+                    <Icon icon="lucide:folder-open" class="size-6" />
+                    <span class="sr-only">Open folder</span>
+                </a>
+        </nav>
+    </div>
+</template>
