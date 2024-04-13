50c079a69d4aed48dac0858488e9d710d0b8cb77
bangnokia <bangnokia@gmail.com>
2024-04-14 00:06:51 +0700

    wip

--- src/Nav.vue
+++ src/Nav.vue
@@ -1,60 +1,20 @@
 <script setup lang=ts>
 import { Icon } from '@iconify/vue';
-import { cn } from './lib/utils'
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipTrigger,
-} from '@/components/ui/tooltip'
-
-export interface LinkProp {
-    title: string
-    label?: string
-    icon: string
-    variant: 'default' | 'ghost'
-}
-
-const links: LinkProp[] = [
-    {
-        title: 'Open Folder',
-        icon: 'lucide:inbox',
-        variant: 'default',
-    },
-    {
-        title: 'Drafts',
-        icon: 'lucide:file',
-        variant: 'ghost',
-    },
-    {
-        title: 'Sent',
-        icon: 'lucide:send',
-        variant: 'ghost',
-    },
-    {
-        title: 'Junk',
-        icon: 'lucide:archive',
-        variant: 'ghost',
-    },
-    {
-        title: 'Trash',
-        icon: 'lucide:trash',
-        variant: 'ghost',
-    },
-    {
-        title: 'Archive',
-        icon: 'lucide:archive',
-        variant: 'ghost',
-    },
-]
+import Button from './components/ui/button/Button.vue';
 </script>
 
 <template>
-    <div class="group flex flex-col gap-4 py-2 data-[collapsed=true]:py-2">
-        <nav class="grid gap-1 px-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
-                <a href="#" class="size-9">
-                    <Icon icon="lucide:folder-open" class="size-6" />
-                    <span class="sr-only">Open folder</span>
-                </a>
+    <div class="group h-screen max-w-12 bg-gray-200 flex flex-col gap-4 py-2">
+        <nav class="flex flex-col items-center justify-center gap-2">
+            <!-- open folder -->
+            <Button variant="link" size="icon">
+                <Icon icon="lucide:folder-open" class="size-6" />
+            </Button>
+
+            <!-- open servers -->
+            <Button variant="link" size="icon">
+                <Icon icon="lucide:server" class="size-6" />
+            </Button>
         </nav>
     </div>
 </template>
