924fc7bfe8cfab7f553dd824fa404b1a713f2c1b
bangnokia <bangnokia@gmail.com>
2024-04-15 11:33:57 +0700

    setup project type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,13 @@
 export type ProjectType = 'local' | 'ssh';
 
 export type Project = {
-
+    id: string;
+    path: string;
+    type: ProjectType;
+    php_binary_path: string;
+    host?: string;
+    port?: number;
+    user?: string;
+    private_key_path?: string;
+    passpharse?: string;
 }
